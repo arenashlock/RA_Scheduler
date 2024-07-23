1c4aff84c164c82e32367ea6cee1ecc5ffcf7ebb
Aren Ashlock <ashlock.aren@gmail.com>
2024-07-23 10:29:03 -0500

    Added RA info table and reading correctly from it

--- main.cpp
+++ main.cpp
@@ -29,11 +29,6 @@ void TEST_print_line(std::string test_line) {
     }
 }
 
-void TEST_assign_availability() {
-    All_School_RAs[0].availability[0][0] = 1;
-    std::cout << All_School_RAs[0].availability[0][0] << std::endl;
-}
-
 void read_availability_file() {
     std::fstream availability_file;
     std::string test;
@@ -70,9 +65,66 @@ void read_schedule_outline_file() {
     }
 }
 
+void build_RA_objects() {
+    std::fstream frederiksen_court_staff_file;
+    std::string staff_member;
+
+    frederiksen_court_staff_file.open("./frederiksen_court_staff.csv", std::fstream::in);
+
+    // File found
+    if(frederiksen_court_staff_file.is_open()) {
+        // Throw out the first line (just labels for data)
+        std::getline(frederiksen_court_staff_file, staff_member);
+
+        for(int i = 0; i < 29; i++) {
+            int start_of_read = 0; int end_of_read = 0;
+
+            // Get the line with the RA's information
+            std::getline(frederiksen_court_staff_file, staff_member);
+
+            // Get the building number of the RA
+            end_of_read = staff_member.find(",");
+            All_School_RAs[i].RA_building_number = stoi(staff_member.substr(start_of_read, (end_of_read - start_of_read)));
+            start_of_read = end_of_read + 1;
+
+            // Get the name of the RA
+            end_of_read = staff_member.find(",", start_of_read);
+            All_School_RAs[i].RA_name = staff_member.substr(start_of_read, (end_of_read - start_of_read));
+            start_of_read = end_of_read + 1;
+
+            // Get the experience of the RA (only need to check first character of last field)
+            if(staff_member[start_of_read] == 'R') {
+                All_School_RAs[i].RA_experience = 3;
+            }
+            else if(staff_member[start_of_read] == 'T') {
+                All_School_RAs[i].RA_experience = 2;
+            }
+            else {
+                All_School_RAs[i].RA_experience = 1;
+            }
+        }
+    }
+    
+    // Cannot read file
+    else {
+        std::cout << "READING NOT WORKING..." << std::endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
     //read_availability_file();
-    read_schedule_outline_file();
+    
+    // Build each RA object for the algorithm
+    build_RA_objects();
+
+    // Test print for ensuring information reading is correct
+    for(int i = 0; i < 29; i++) {
+        std::cout << "Name: " << All_School_RAs[i].RA_name << std::endl <<
+        "Building: " << All_School_RAs[i].RA_building_number << std::endl <<
+        "Experience: " << All_School_RAs[i].RA_experience << std::endl;
+    }
+
+    //read_schedule_outline_file();
 
     return 0;
 }
--- main.h
+++ main.h
@@ -12,10 +12,10 @@ extern const int NEW_STAFF = 1;
 
 class RA {
 public:
-    int building_number;
-    std::string name;
-    int experience;
-    int availability[7][10];
+    int RA_building_number;
+    std::string RA_name;
+    int RA_experience;
+    int RA_availability[7][10];
 };
 
 extern RA All_School_RAs[FREDDY_SCHOOL_RAS];
